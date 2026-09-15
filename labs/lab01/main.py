fbacd6a6a8eefa852a466e8bc2febbb09dc5024d
Діана Білянич <dianabilanic@Noutbuk-Diana.local>
2026-09-15 13:02:05 +0300

    Додано вивід даних студента в main.py

--- labs/lab01/main.py
+++ labs/lab01/main.py
@@ -1,14 +1,27 @@
 """Головний файл для запуску Лабораторної роботи №1."""
 
-from task1 import analyze_passwords
-from task2 import check_access
-from task3 import run_task3
+import os
+import sys
+
+# Додаємо шлях до кореня проекту для імпорту shared
+project_root = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "../../"),
+)
+sys.path.append(project_root)
+
+from shared import student  # noqa: E402
+from task1 import analyze_passwords  # noqa: E402
+from task2 import check_access  # noqa: E402
+from task3 import run_task3  # noqa: E402
 
 
 def main():
     """Точка входу для запуску всіх завдань."""
     print("=========================================")
-    print("Початок виконання Лабораторної роботи №1")
+    print("Лабораторна робота №1")
+    print(f"Виконала: {student.STUDENT_NAME}")
+    print(f"Група: {student.GROUP_NAME}")
+    print(f"Варіант: {student.VARIANT_NUMBER}")
     print("=========================================")
 
     # Виклик Завдання 1
